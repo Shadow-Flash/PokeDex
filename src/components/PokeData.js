e42e6f52d95b731ec0c6ab1b7a746a6efcf71b42
Shadow-Flash <varung848@gmail.com>
2021-02-04 01:28:12 +0530

    Card Page Done

--- src/components/PokeData.js
+++ src/components/PokeData.js
@@ -6,6 +6,7 @@ const PokeData = () => {
   const [pokemons, setPokemon] = useState([]);
   const [nextPage, setNextPage] = useState("");
   const [prevPage, setPrevPage] = useState("");
+  const [wait, setWait] = useState(true);
 
   useEffect(() => {
     const getPokemon = async () => {
@@ -13,6 +14,7 @@ const PokeData = () => {
       setNextPage(response.data.next);
       setPrevPage(response.data.previous);
       await fetchPokemon(response.data.results);
+      setWait(false);
     };
     getPokemon();
   }, []);
@@ -28,37 +30,53 @@ const PokeData = () => {
   };
 
   const nextUrl = async () => {
+    setWait(true);
     await axios.get(nextPage).then(async (ndata) => {
       await fetchPokemon(ndata.data.results);
       setNextPage(ndata.data.next);
       setPrevPage(ndata.data.previous);
+      setWait(false);
     });
   };
 
   const prevUrl = async () => {
+    if (!prevPage) return;
+    setWait(true);
     await axios.get(prevPage).then(async (pdata) => {
       await fetchPokemon(pdata.data.results);
       setNextPage(pdata.data.next);
       setPrevPage(pdata.data.previous);
+      setWait(false);
     });
   };
 
   return (
     <div className="ui container">
       <h1>PokeDex</h1>
-      <div className="ui two column grid">
-        {pokemons.map((pokemon, id) => {
-          return <PokeCard key={id} pokemon={pokemon} />;
-        })}
-      </div>
-      <div className="ui grid container">
-        <a className="circular ui tiny icon button " onClick={prevUrl}>
-          <i className="left arrow icon"></i>
-        </a>
-        <a className=" circular ui tiny right icon button" onClick={nextUrl}>
-          <i className="right arrow icon"></i>
-        </a>
-      </div>
+      {wait ? (
+        <div className="ui active inverted dimmer">
+          <div className="ui large text loader">Wait Pokemon's Loading</div>
+        </div>
+      ) : (
+        <>
+          <div className="ui two column grid">
+            {pokemons.map((pokemon, id) => {
+              return <PokeCard key={id} pokemon={pokemon} />;
+            })}
+          </div>
+          <div className="ui grid container">
+            <a className="circular ui tiny icon button " onClick={prevUrl}>
+              <i className="left arrow icon"></i>
+            </a>
+            <a
+              className=" circular ui tiny right icon button"
+              onClick={nextUrl}
+            >
+              <i className="right arrow icon"></i>
+            </a>
+          </div>
+        </>
+      )}
     </div>
   );
 };
